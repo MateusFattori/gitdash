1453f0970b12f51c60daa7210c649e05e280d6f4
Mateus Fattori <83764798+MateusFattori@users.noreply.github.com>
2024-10-11 08:39:02 -0300

    Add files via upload

--- src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
@@ -1,26 +1,20 @@
 package br.com.fiap.gitdash.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
                 .anyRequest().authenticated()
             )
-            .oauth2Login()
-            .and()
-            .logout(logout -> logout
-                .logoutSuccessUrl("/")
-                .permitAll()
+            .oauth2Login(oauth2 -> oauth2
+                .loginPage("/oauth2/authorization/github")
             );
-
-        return http.build();
     }
 }
